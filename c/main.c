1163aaa3ee1e6b6b1f272bd2967f16af79ff99ce
Ryan Dimsey <ryandimsey@gmail.com>
2023-04-12 17:37:59 +1000

    Some refactors to clean code

--- c/main.c
+++ c/main.c
@@ -1,7 +1,71 @@
 #include <arpa/nameser.h>
 #include <resolv.h>
 #include <string.h>
+#include <stdbool.h>
 
+enum { MAX_FLAGS_STR = 8, // TODO WHAT IS THE ACTUAL MAX?
+       MAX_REGEX_STR = 128,
+       MAX_SERVICE_STR = 128 };
+
+typedef struct {
+    int order;
+    int preference;
+    char flags[MAX_FLAGS_STR];
+    char service[MAX_SERVICE_STR];
+    char regex[MAX_REGEX_STR];
+    char replacement[NS_MAXDNAME];
+} naptr_resource_record;
+
+bool parse_naptr_resource_record(const unsigned char * buf, uint16_t buf_sz, naptr_resource_record * const nrr) {
+    bool success = false;
+
+    if ((0 != buf) &&
+        (0 != nrr))
+    {
+        size_t bytes_consumed = 0;
+        int flags_len = 0;
+        int service_len = 0;
+        int regex_len = 0;
+
+        nrr->order = ns_get16(&buf[bytes_consumed]);
+        bytes_consumed += 2;
+
+        nrr->preference = ns_get16(&buf[bytes_consumed]);
+        bytes_consumed += 2;
+
+        flags_len = buf[bytes_consumed];
+        bytes_consumed += 1;
+
+        memcpy(nrr->flags, &buf[bytes_consumed], flags_len);
+        bytes_consumed += flags_len;
+
+        service_len = buf[bytes_consumed];
+        bytes_consumed += 1;
+
+        memcpy(nrr->service, &buf[bytes_consumed], service_len);
+        bytes_consumed += service_len;
+
+        regex_len = buf[bytes_consumed];
+        bytes_consumed += 1;
+
+        memcpy(nrr->regex, &buf[bytes_consumed], regex_len);
+        bytes_consumed += regex_len;
+
+        int bytes_uncompressed = ns_name_uncompress(
+            &buf[0],              /* Start of compressed buffer */
+            &buf[buf_sz],         /* End of compressed buffer */
+            &buf[bytes_consumed], /* Where to start decompressing */
+            nrr->replacement,     /* Where to store decompressed value */
+            NS_MAXDNAME           /* Number of bytes that can be stored in output buffer */
+        );
+
+        if (0 <= bytes_uncompressed) {
+            success = true;
+        }        
+    }
+
+    return success;
+}
 
 void foo(const char* dname) {
     unsigned char answer[NS_PACKETSZ];
@@ -20,72 +84,33 @@ void foo(const char* dname) {
     // parse response and process NAPTR records
     ns_initparse(answer, bytes_received, &handle);
     count = ns_msg_count(handle, ns_s_an);
-    printf("Got back %i results for query %s:\n", count, dname);
+    printf("\n\nGot back %i results for query %s:\n", count, dname);
 
     for (int i = 0; i < count; i++) {
         ns_parserr(&handle, ns_s_an, i, &rr);
         if (ns_rr_type(rr) == ns_t_naptr) {
-            size_t bytes_consumed = 0;
-
-            int order = ns_get16(&rr.rdata[bytes_consumed]);
-            bytes_consumed += 2;
-
-            int preference = ns_get16(&rr.rdata[bytes_consumed]);
-            bytes_consumed += 2;
-
-            int flags_len = ns_rr_rdata(rr)[bytes_consumed];
-            bytes_consumed += 1;
-
-            char flags[8] = "";
-            memcpy(flags, &rr.rdata[bytes_consumed], flags_len);
-            bytes_consumed += flags_len;
-
-            int service_len = ns_rr_rdata(rr)[bytes_consumed];
-            bytes_consumed += 1;
-
-            char service[128] = "";
-            memcpy(service, &rr.rdata[bytes_consumed], service_len);
-            bytes_consumed += service_len;
-
-            int regex_len = ns_rr_rdata(rr)[bytes_consumed];
-            bytes_consumed += 1;
-
-            char regex[128] = "";
-            memcpy(regex, &rr.rdata[bytes_consumed], regex_len);
-            bytes_consumed += regex_len;
-
-            char replacement[NS_MAXDNAME] = "";
-            int bytes_uncompressed = ns_name_uncompress(
-                &rr.rdata[0],              /* Start of compressed buffer */
-                &rr.rdata[rr.rdlength],    /* End of compressed buffer */
-                &rr.rdata[bytes_consumed], /* Where to start decompressing */
-                replacement,               /* Where to store decompressed value */
-                NS_MAXDNAME                /* Number of bytes that can be stored in output buffer */
-            );
-
-            if (-1 == bytes_uncompressed) {
-                // throw error?
-            }
+            naptr_resource_record nrr = {};
+            parse_naptr_resource_record(&rr.rdata[0], rr.rdlength, &nrr);
 
             printf("\nResult:\n");
-            printf("rdata.order: %i\n",        order);
-            printf("rdata.preference: %i\n",   preference);
-            printf("rdata.flags: '%s'\n",      flags);
-            printf("rdata.service: '%s'\n",    service);
-            printf("rdata.regexp: '%s'\n",     regex);
-            printf("replacement: '%s'\n",     replacement);
+            printf("rdata.order: %i\n",        nrr.order);
+            printf("rdata.preference: %i\n",   nrr.preference);
+            printf("rdata.flags: '%s'\n",      nrr.flags);
+            printf("rdata.service: '%s'\n",    nrr.service);
+            printf("rdata.regexp: '%s'\n",     nrr.regex);
+            printf("replacement: '%s'\n",      nrr.replacement);
         }
     }
 }
 
 
 int main() {
-    // const char* dname1 = "internet.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
-    // const char* dname2 = "internet.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
+    const char* dname1 = "internet.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
+    const char* dname2 = "internet.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
     const char* dname3 = "mms.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
 
-    // foo(dname1);
-    // foo(dname2);
+    foo(dname1);
+    foo(dname2);
     foo(dname3);
 
     return 0;
