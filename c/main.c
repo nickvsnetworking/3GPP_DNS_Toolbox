231b8f75ee467585b5f28d73c0e66f130e332652
Ryan Dimsey <ryandimsey@gmail.com>
2023-04-13 10:22:03 +1000

    Before dname split into target, interface, protocol, apn, mnc ...

--- c/main.c
+++ c/main.c
@@ -6,7 +6,14 @@
 
 enum { MAX_FLAGS_STR = 8, // TODO WHAT IS THE ACTUAL MAX?
        MAX_REGEX_STR = 128,
-       MAX_SERVICE_STR = 128 };
+       MAX_SERVICE_STR = 128,
+       MAX_REPLACEMENT_STR = NS_MAXDNAME };
+
+enum { ORDER_SZ_BYTES = 2,
+       PREFERENCE_SZ_BYTES = 2,
+       FLAGS_LEN_SZ_BYTES = 1,
+       SERVICE_LEN_SZ_BYTES = 1,
+       REGEX_LEN_SZ_BYTES = 1 };
 
 
 typedef struct {
@@ -15,46 +22,58 @@ typedef struct {
     char flags[MAX_FLAGS_STR];
     char service[MAX_SERVICE_STR];
     char regex[MAX_REGEX_STR];
-    char replacement[NS_MAXDNAME];
+    char replacement[MAX_REPLACEMENT_STR];
 } naptr_resource_record;
 
 
 static bool parse_naptr_resource_record(const unsigned char * buf, uint16_t buf_sz, naptr_resource_record * const nrr);
-static void parse_only_naptr_resource_records(ns_msg * const handle, int count);
+static int parse_only_naptr_resource_records(ns_msg * const handle, int count, naptr_resource_record * const out_buf, size_t out_buf_sz);
 
 
-void resolve_apn(const char* dname) {
+int resolve_apn(const char* dname, naptr_resource_record * const out_buf, size_t out_buf_sz) {
     unsigned char answer[NS_PACKETSZ];
     ns_msg handle;
     int bytes_received;
     int count;
+    int num_nrrs;
 
-    // perform NAPTR lookup
+    /* Perform NAPTR lookup */
     bytes_received = res_query(dname, ns_c_in, ns_t_naptr, answer, sizeof(answer));
-
     if (bytes_received <= 0) {
-        return;
+        return 0;
     }
 
-    // parse response and process NAPTR records
+    /* Parse response and process NAPTR records */
     ns_initparse(answer, bytes_received, &handle);
     count = ns_msg_count(handle, ns_s_an);
+    num_nrrs = parse_only_naptr_resource_records(&handle, count, out_buf, out_buf_sz);
 
-    parse_only_naptr_resource_records(&handle, count); // todo all output array
-
-    printf("\n\nGot back %i results for query %s:\n", count, dname);
-
+    return num_nrrs;
 }
 
 
 int main() {
+    enum { NRR_BUF_SZ = 16 };
+    naptr_resource_record nrr_buf[NRR_BUF_SZ] = {};
+    int elements_consumed = 0;
+
     const char* dname1 = "internet.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
     const char* dname2 = "internet.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
     const char* dname3 = "mms.apn.epc.mnc001.mcc001.3gppnetwork.org.nickvsnetworking.com";
 
-    resolve_apn(dname1);
-    resolve_apn(dname2);
-    resolve_apn(dname3);
+    // elements_consumed += resolve_apn(dname1, &nrr_buf[elements_consumed], NRR_BUF_SZ - elements_consumed);
+    elements_consumed += resolve_apn(dname2, &nrr_buf[elements_consumed], NRR_BUF_SZ - elements_consumed);
+    // elements_consumed += resolve_apn(dname3, &nrr_buf[elements_consumed], NRR_BUF_SZ - elements_consumed);
+
+    for (int i = 0; i < elements_consumed; ++i) {
+        printf("\nResult[%i]:\n", i);
+        printf("rdata.order: %i\n",        nrr_buf[i].order);
+        printf("rdata.preference: %i\n",   nrr_buf[i].preference);
+        printf("rdata.flags: '%s'\n",      nrr_buf[i].flags);
+        printf("rdata.service: '%s'\n",    nrr_buf[i].service);
+        printf("rdata.regexp: '%s'\n",     nrr_buf[i].regex);
+        printf("replacement: '%s'\n",      nrr_buf[i].replacement);
+    }
 
     return 0;
 }
@@ -66,31 +85,31 @@ static bool parse_naptr_resource_record(const unsigned char * buf, uint16_t buf_
     if ((0 != buf) &&
         (0 != nrr))
     {
+        int flags_len;
+        int service_len;
+        int regex_len;
         size_t bytes_consumed = 0;
-        int flags_len = 0;
-        int service_len = 0;
-        int regex_len = 0;
 
         nrr->order = ns_get16(&buf[bytes_consumed]);
-        bytes_consumed += 2;
+        bytes_consumed += ORDER_SZ_BYTES;
 
         nrr->preference = ns_get16(&buf[bytes_consumed]);
-        bytes_consumed += 2;
+        bytes_consumed += PREFERENCE_SZ_BYTES;
 
         flags_len = buf[bytes_consumed];
-        bytes_consumed += 1;
+        bytes_consumed += FLAGS_LEN_SZ_BYTES;
 
         memcpy(nrr->flags, &buf[bytes_consumed], flags_len);
         bytes_consumed += flags_len;
 
         service_len = buf[bytes_consumed];
-        bytes_consumed += 1;
+        bytes_consumed += SERVICE_LEN_SZ_BYTES;
 
         memcpy(nrr->service, &buf[bytes_consumed], service_len);
         bytes_consumed += service_len;
 
         regex_len = buf[bytes_consumed];
-        bytes_consumed += 1;
+        bytes_consumed += REGEX_LEN_SZ_BYTES;
 
         memcpy(nrr->regex, &buf[bytes_consumed], regex_len);
         bytes_consumed += regex_len;
@@ -100,7 +119,7 @@ static bool parse_naptr_resource_record(const unsigned char * buf, uint16_t buf_
             &buf[buf_sz],         /* End of compressed buffer */
             &buf[bytes_consumed], /* Where to start decompressing */
             nrr->replacement,     /* Where to store decompressed value */
-            NS_MAXDNAME           /* Number of bytes that can be stored in output buffer */
+            MAX_REPLACEMENT_STR   /* Number of bytes that can be stored in output buffer */
         );
 
         if (0 <= bytes_uncompressed) {
@@ -112,23 +131,23 @@ static bool parse_naptr_resource_record(const unsigned char * buf, uint16_t buf_
 }
 
 
-static void parse_only_naptr_resource_records(ns_msg * const handle, int count) {
+static int parse_only_naptr_resource_records(ns_msg * const handle, int count, naptr_resource_record * const out_buf, size_t out_buf_sz) {
     ns_rr rr;
+    int num_nrrs = 0;
+
+    if ((0 == handle)  ||
+        (0 == out_buf) ||
+        (out_buf_sz < count)) {
+        return num_nrrs;
+    }
 
     for (int i = 0; i < count; i++) {
         ns_parserr(handle, ns_s_an, i, &rr);
         if (ns_rr_type(rr) == ns_t_naptr) {
-            naptr_resource_record nrr = {};
-            parse_naptr_resource_record(&rr.rdata[0], rr.rdlength, &nrr);
-
-            printf("\nResult:\n");
-            printf("rdata.order: %i\n",        nrr.order);
-            printf("rdata.preference: %i\n",   nrr.preference);
-            printf("rdata.flags: '%s'\n",      nrr.flags);
-            printf("rdata.service: '%s'\n",    nrr.service);
-            printf("rdata.regexp: '%s'\n",     nrr.regex);
-            printf("replacement: '%s'\n",      nrr.replacement);
+            parse_naptr_resource_record(&rr.rdata[0], rr.rdlength, &out_buf[num_nrrs]);
+            ++num_nrrs;
         }
     }
 
+    return num_nrrs;
 }
